73792bdc7a4b85814cb29321362f2e6bcc306e3b
yoma75 <fredyomayuza@gmail.com>
2024-12-11 20:32:07 -0500

    creacion proyecto

---
+++ assets/js/botonFlotante.js
@@ -0,0 +1,8 @@
+// Boton flotante
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // Desplazamiento suave
+  });
+}
+
---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+// icono menu barras
+let navbar = document.querySelector(".navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+
+// icono buscador
+let searchForm = document.querySelector(".search-form");
+
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+  navbar.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+
+
+// Boton flotante
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // Desplazamiento suave
+  });
+}
